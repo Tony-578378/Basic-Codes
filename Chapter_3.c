025d9c235ebdcbfb2265df715e9378e52a744b5b
YIXUAN TANG <1659120060@qq.com>
2026-04-10 00:35:29 +0100

    add Chapter_3.c

--- Chapter_3.c
+++ Chapter_3.c
@@ -170,3 +170,18 @@ int main(void)
    printf("%d\n", c);
    return 0;
 }
+
+/* 3.12 */
+/* Decimal, hexadecimal, octal conversions. */
+#include <stdio.h>
+
+int main(void)
+{
+   printf("%d  %x  %o\n", 19, 19, 19); /* 19 13 23 */
+   printf("%d  %x  %o\n", 0x1c, 0x1c, 0x1c); /* 28 1c 34 */
+   printf("%d  %x  %o\n", 017, 017, 017); /* 15 f 17 */
+   printf("%d\n", 11 + 0x11 + 011); /* 11 + 17 + 9 = 37 */
+   printf("%x\n", 2097151); /* 1fffff */
+   printf("%d\n", 0x1FfFFf); /* 2097151 */
+   return 0;
+}
