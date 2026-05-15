0b886e97153d0607dad3a9e5168133190f2cbc06
YIXUAN TANG <1659120060@qq.com>
2026-05-15 23:21:00 +0100

    Add Chapter_4.c

---
+++ Chapter_4.c
@@ -0,0 +1,31 @@
+/* 4.2 */
+#include <stdio.h>
+
+int main(void)
+{
+    int  j = 7;
+
+    printf("%d\n", 3 < j < 5);  /* 1(True) gets printed, not 0(False). Because 3 < j < 5 is equivalent to (3 < j) < 5, and 3 < j means 3 < 7, which is true(1), so the whole expression is equivalent to 1 < 5, which is true(1). */
+    return 0;
+}
+/* The correct way to write a test for 3 < j < 5: */
+#include <stdio.h>
+
+int main(void)
+{
+    int  j = 7;
+
+    printf("%d\n", 3 < j && j < 5);/* 0 gets printed, as this expression is equivalent to (3 < j) && (j < 5). */
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(void)
+{
+    double  x = 7e+33, y = 0.001;
+
+    printf("%d\n", x < x + y);
+    printf("%d\n", (x - (x + y)) < 0.0); /* Both 0s get printed instead of 1s. Because though mathematically the statements are true, x and x + y are equal in terms of machine accuracy. */
+    return 0;
+}
