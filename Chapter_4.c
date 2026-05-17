51b3291301669c3b6fca003188d6da7107662f71
YIXUAN TANG <1659120060@qq.com>
2026-05-18 00:37:39 +0100

    Update Chapter_4.c

--- Chapter_4.c
+++ Chapter_4.c
@@ -29,3 +29,32 @@ int main(void)
     printf("%d\n", (x - (x + y)) < 0.0); /* Both 0s get printed instead of 1s. Because though mathematically the statements are true, x and x + y are equal in terms of machine accuracy. */
     return 0;
 }
+
+/* 4.4 */
+#include <stdio.h>
+
+int main(void)
+{
+    int  cnt = 0, c;
+    while (++cnt <= 3 && (c = getchar()) != EOF) {
+        printf("Character %d: %c\n", cnt, c);
+    }
+    return 0;
+}
+/* This is an example of a short-cut evaluation. If the length of the input is >= 3, then only the first 3 characters will be output, since the first expression holds. Otherwise, all characters will be output, since the second expression holds. */
+
+/* 4.5 */
+/* Compound statement */
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 1, b = 2, c = 3;
+    
+    if (a > 0) {
+    a += b += c;
+    printf("a = %d, b = %d, c = %d\n", a, b, c);/* a = 6, b = 5, c = 3 */
+    }
+
+    return 0;
+}
