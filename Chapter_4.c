462c6075ecbd78e26019ab7b78ad10503451b897
YIXUAN TANG <1659120060@qq.com>
2026-06-21 00:53:03 +0100

    Update Chapter_4.c

--- Chapter_4.c
+++ Chapter_4.c
@@ -418,3 +418,38 @@ int main(void)
     return 0;
 }
 /* an infinite output of 12431243... */
+
+/* 16 */
+#include <stdio.h>
+
+int main(void)
+{
+    int b1, b2, b3, b4, b5, b6, b7;
+    int cnt = 0;
+    int majority_count = 0;
+
+    printf("\n%5s%5s%5s%5s%5s%5s%5s%5s%11s\n\n",
+           "Cnt", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "majority"); /* boolean variables */
+
+    for (b1 = 0; b1 <= 1; ++b1)
+        for (b2 = 0; b2 <= 1; ++b2)
+            for (b3 = 0; b3 <= 1; ++b3)
+                for (b4 = 0; b4 <= 1; ++b4)
+                    for (b5 = 0; b5 <= 1; ++b5)
+                        for (b6 = 0; b6 <= 1; ++b6)
+                            for (b7 = 0; b7 <= 1; ++b7) {
+                                int majority =
+                                    b1 + b2 + b3 + b4 + b5 + b6 + b7 >= 4;
+
+                                if (majority)
+                                    majority_count++;
+
+                                printf("%5d%5d%5d%5d%5d%5d%5d%5d%9d\n",
+                                       ++cnt, b1, b2, b3, b4, b5, b6, b7,
+                                       majority);
+                            }
+
+    printf("\nNumber with majority value 1: %d\n", majority_count); /* 64 out of 128 */
+
+    return 0;
+}
