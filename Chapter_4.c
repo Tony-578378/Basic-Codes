cf81b54b21f1f4dee5ecebef60672aad2d3ffa31
YIXUAN TANG <1659120060@qq.com>
2026-07-07 23:03:02 +0100

    Update Chapter_4.c

--- Chapter_4.c
+++ Chapter_4.c
@@ -984,3 +984,17 @@ int main(void)
     return 0;
 }
 /* only warnings, no errors */
+
+/* 35 */
+#include <stdio.h>
+
+int main(void)
+{
+    int   L = -3;                        /* L is a variable */
+    if (L < 0)
+       goto L;
+    printf("L = %d\n", L); /* this is skipped */
+    L: printf("Exiting label test!\n");  /* L is a label */
+
+    return 0;
+}
